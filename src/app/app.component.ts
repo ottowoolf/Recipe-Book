46694439d56b3dcecf537a53694404e4d022916c
otto <otto.woolf@gmail.com>
2022-02-06 17:22:02 +1000

    modified app to use Observables instead of Emitters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,4 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'recipe-book-angular';
   loadedfeature = 'recipe';
-
-  onNavigate(feature: string) {
-    this.loadedfeature = feature;
-  }
 }

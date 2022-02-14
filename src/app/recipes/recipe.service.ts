38819cb643cecda00a5b4e37bd384c3e2d71a06b
otto <otto.woolf@gmail.com>
2022-02-14 10:51:24 +1000

    store/fetch data on firebase and using resolver

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -7,31 +7,37 @@ import { Recipe } from './recipe.model';
 @Injectable()
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
+  private recipes = [];
   constructor(private slService: ShoppingListService) {}
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Wagyu Burger',
-      'Wagyu Burger with Aged cheddar, lettuce and a side of fries',
-      'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2012/6/1/2/FNM_070112-Copy-That-Almost-Famous-Animal-Style-Burger-Recipe_s4x3.jpg.rend.hgtvcom.616.462.suffix/1382541460839.jpeg',
-      [
-        new Ingredient('Beef', 2),
-        new Ingredient('Buns', 2),
-        new Ingredient('Lettuce', 1),
-        new Ingredient('Cheddar', 2),
-      ]
-    ),
-    new Recipe(
-      'Seafood Platter',
-      'Seafood Platter With Mango Salsa & Horseradish Mayo',
-      'https://foodhub.scene7.com/is/image/woolworthsltdprod/2112-seafood-platter-with-mango-salsa-and-horseradish-mayo:Square-1300x1300',
-      [
-        new Ingredient('Prawns', 8),
-        new Ingredient('Lobster', 1),
-        new Ingredient('Oysters', 6),
-      ]
-    ),
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Wagyu Burger',
+  //     'Wagyu Burger with Aged cheddar, lettuce and a side of fries',
+  //     'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2012/6/1/2/FNM_070112-Copy-That-Almost-Famous-Animal-Style-Burger-Recipe_s4x3.jpg.rend.hgtvcom.616.462.suffix/1382541460839.jpeg',
+  //     [
+  //       new Ingredient('Beef', 2),
+  //       new Ingredient('Buns', 2),
+  //       new Ingredient('Lettuce', 1),
+  //       new Ingredient('Cheddar', 2),
+  //     ]
+  //   ),
+  //   new Recipe(
+  //     'Seafood Platter',
+  //     'Seafood Platter With Mango Salsa & Horseradish Mayo',
+  //     'https://foodhub.scene7.com/is/image/woolworthsltdprod/2112-seafood-platter-with-mango-salsa-and-horseradish-mayo:Square-1300x1300',
+  //     [
+  //       new Ingredient('Prawns', 8),
+  //       new Ingredient('Lobster', 1),
+  //       new Ingredient('Oysters', 6),
+  //     ]
+  //   ),
+  // ];
+
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
   getRecipes() {
     return this.recipes.slice();

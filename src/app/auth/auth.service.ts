30d50da7d8e2eece6909ba850d967e17b1f90585
otto <otto.woolf@gmail.com>
2022-02-17 22:31:43 +1000

    refactor login and signup

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, throwError } from 'rxjs';
+import { catchError, Subject, tap, throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { User } from './user.model';
 
 export interface IAuthResponseData {
   idToken: string;
@@ -14,6 +15,8 @@ export interface IAuthResponseData {
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
+  user = new Subject<User>();
+
   constructor(private http: HttpClient) {}
   apiKey = environment.API_KEY;
   signup(email: string, password: string) {
@@ -22,7 +25,17 @@ export class AuthService {
         `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${this.apiKey}`,
         { email: email, password: password, returnSecureToken: true }
       )
-      .pipe(catchError(this.handleError));
+      .pipe(
+        catchError(this.handleError),
+        tap((resData) => {
+          this.handleAuthentication(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          );
+        })
+      );
   }
   login(email: string, password: string) {
     return this.http
@@ -30,12 +43,32 @@ export class AuthService {
         `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${this.apiKey}`,
         { email: email, password: password, returnSecureToken: true }
       )
-      .pipe(catchError(this.handleError));
+      .pipe(
+        catchError(this.handleError),
+        tap((resData) => {
+          this.handleAuthentication(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          );
+        })
+      );
+  }
+  private handleAuthentication(
+    email: string,
+    userId: string,
+    token: string,
+    expiresIn: number
+  ) {
+    const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
+    const user = new User(email, userId, token, expirationDate);
+    this.user.next(user);
   }
   private handleError(errorResponse: HttpErrorResponse) {
     let errorMessage = 'An Unknown Error occured!';
     if (!errorResponse.error || !errorResponse.error.error) {
-      return throwError(() => new Error(errorMessage));
+      return throwError(errorMessage);
     }
     switch (errorResponse.error.error.message) {
       case 'EMAIL_EXISTS':
@@ -47,6 +80,6 @@ export class AuthService {
       case 'INVALID_PASSWORD':
         errorMessage = 'Incorrect password.';
     }
-    return throwError(() => new Error(errorMessage));
+    return throwError(errorMessage);
   }
 }
